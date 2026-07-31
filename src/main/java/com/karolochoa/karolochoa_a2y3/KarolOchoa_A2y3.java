abc1e4564600fc7c594c6a7c55be1b2d0dd93c1e
karol <karol@Koryy>
2026-07-30 19:38:34 -0700

    Proyecto Banco Méxicano parte 1 Karol Ochoa Beltran

---
+++ src/main/java/com/karolochoa/karolochoa_a2y3/KarolOchoa_A2y3.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.karolochoa.karolochoa_a2y3;
+
+/**
+ *
+ * @author karol
+ */
+public class KarolOchoa_A2y3 {
+
+    
+}
